db7b8e3dea9a78d443a1c527f5dd5378c310560c
MichaelLu2003 <122946254+MichaelLu2003@users.noreply.github.com>
2025-05-29 21:38:02 +0800

    Update emmet_main.py

--- emmet/emmet_main.py
+++ emmet/emmet_main.py
@@ -218,8 +218,8 @@ def execute_emmet(
         )
 
         #add optimization hyper-parameters
-        if hparams.mom2_update_weight != 1:
-            cov *= hparams.mom2_update_weight
+        # if hparams.mom2_update_weight != 1:
+        #     cov *= hparams.mom2_update_weight
 
         if hparams.update_norm_lambda != 0:
             cov += hparams.update_norm_lambda * torch.eye(cov.shape[0], dtype=cov.dtype, device = cov.device)
